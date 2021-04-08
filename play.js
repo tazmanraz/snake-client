d8ea8b37f556e82a2bcde568c28f6eb204a719a5
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-08 01:34:37 +0000

    updated play.js to take inputs

--- play.js
+++ play.js
@@ -3,15 +3,26 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
-
+const stdin = process.stdin;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-//  const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-//   return stdin;
-// }
+ const setupInput = function() {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
+setupInput();
