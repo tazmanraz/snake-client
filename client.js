0362e774f44d6ae698db417eb569964409c2ba67
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-08 01:10:12 +0000

    adding moving with settimeout and setinterval

--- client.js
+++ client.js
@@ -19,6 +19,26 @@ const connect = function() {
     conn.write('Name: MTS');
   });
 
+  // conn.on('connect', (data) => {
+  //   console.log(`moving up`);
+  //   conn.write('Move: down');
+  //   conn.write('Move: left');
+
+  // });
+
+  // conn.on('connect', () => {
+  //   for (let i = 0; i < 5; i++) {
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+  //     }, i * 50);
+  //   }
+  // });
+
+  // conn.on('connect', () => {
+  //   let moving = setInterval( ()=> { conn.write('Move: up'); }, 100);
+  //   setTimeout( () => { clearInterval(moving) }, 1000);
+  // })
+
   return conn;
 }
 
