d7cbd5ca4f8a22765929929d71a29910219636d1
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-08 02:04:58 +0000

    added wasd movement commands

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection; 
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,10 +12,22 @@ const setupInput = function() {
 
 const handleUserInput = function() {
   process.stdin.on('data', (key) => {
-    process.stdout.write('.');
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
+
   });
 }
 
--- play.js
+++ play.js
@@ -3,28 +3,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-//  const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-//   handleUserInput();
-//   return stdin;
-// }
-
-// const handleUserInput = function() {
-//   process.stdin.on('data', (key) => {
-//     process.stdout.write('.');
-//     if (key === '\u0003') {
-//       process.exit();
-//     }
-//   });
-// }
-
-setupInput();
+setupInput(connect());
